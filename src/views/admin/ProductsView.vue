3ed177547cbfcfbcaa2d560ab07e3ded4a8edc86
DrubberSan16 <135788277+DrubberSan16@users.noreply.github.com>
2026-02-26 23:42:12 -0500

    feat: agregar módulos de mantenimiento completos y CRUD genérico

---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <MaintenanceCrudView module-key="productos" />
+</template>
+
+<script setup lang="ts">
+import MaintenanceCrudView from "@/views/admin/MaintenanceCrudView.vue";
+</script>
